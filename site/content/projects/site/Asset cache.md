34bcf9849c694d1837defe95da0700040a3de6d1
olehsm <ohsmyl@gmail.com>
2020-03-11 20:08:18 +0100

    PWA part 2 asset cache

---
+++ site/content/projects/site/Asset cache.md
@@ -0,0 +1,16 @@
++++
+author = "Ole Halvor Smylingsås"
+description = ""
+pageresources = []
+categories = []
+tags = ["PWA", "JS"]     
+slug = ""
+title = "Asset Cache"
+date = 2020-03-11T20:06:24+01:00
+draft = "false"
+featuretext = ""
+featureimg = ""
+comments = "false"
++++
+
+https://www.freecodecamp.org/news/build-a-pwa-from-scratch-with-html-css-and-javascript/
--- site/static/service-worker.js
+++ site/static/service-worker.js
@@ -1,1 +1,27 @@
-self.addEventListener("fetch", event => {})
+const staticSmylingsas = "smylingsas-net-asset-cache-v1"
+
+
+const assets = [
+  "/",
+  "/index.html",
+  "/style/style.css",
+  "/js/app.js",
+]
+
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticSmylingsas).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
