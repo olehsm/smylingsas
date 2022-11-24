b9691c377ecae44c5da34b25b6800c79d6feedda
Ole Halvor Smylingsås <ohsmyl@gmail.com>
2022-11-24 21:50:41 +0100

    ADded shortcut icons, better a11y colors etc

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,69 +1,69 @@
-// self.addEventListener("install", event => {
-// 	event.waitUntil(caches.open("offline-fallbacks")
-// 		.then(cache => cache.add("offline.html"))
-// 	);
-// });
+self.addEventListener("install", event => {
+	event.waitUntil(caches.open("offline-fallbacks")
+		.then(cache => cache.add("offline.html"))
+	);
+});
 
 
-// self.addEventListener("fetch", event => {
-// 	if(event.request.mode === "navigate") {
-// 		event.respondWith(fetch(event.request)
-// 				.catch(error => {
-// 					return caches.open("offline-fallbacks").then(cache => cache.match("offline.html"));
-// 				})
-// 		);
-// 	}
-// });
+self.addEventListener("fetch", event => {
+	if(event.request.mode === "navigate") {
+		event.respondWith(fetch(event.request)
+				.catch(error => {
+					return caches.open("offline-fallbacks").then(cache => cache.match("offline.html"));
+				})
+		);
+	}
+});
 
 // This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
-const CACHE = "pwabuilder-offline-page";
+// const CACHE = "pwabuilder-offline-page";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const offlineFallbackPage = "offline.html";
+// const offlineFallbackPage = "offline.html";
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
+// self.addEventListener("message", (event) => {
+//   if (event.data && event.data.type === "SKIP_WAITING") {
+//     self.skipWaiting();
+//   }
+// });
 
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-  );
-});
+// self.addEventListener('install', async (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE)
+//       .then((cache) => cache.add(offlineFallbackPage))
+//   );
+// });
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
+// if (workbox.navigationPreload.isSupported()) {
+//   workbox.navigationPreload.enable();
+// }
 
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
+// workbox.routing.registerRoute(
+//   new RegExp('/*'),
+//   new workbox.strategies.StaleWhileRevalidate({
+//     cacheName: CACHE
+//   })
+// );
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
+// self.addEventListener('fetch', (event) => {
+//   if (event.request.mode === 'navigate') {
+//     event.respondWith((async () => {
+//       try {
+//         const preloadResp = await event.preloadResponse;
 
-        if (preloadResp) {
-          return preloadResp;
-        }
+//         if (preloadResp) {
+//           return preloadResp;
+//         }
 
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
+//         const networkResp = await fetch(event.request);
+//         return networkResp;
+//       } catch (error) {
 
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
-});
+//         const cache = await caches.open(CACHE);
+//         const cachedResp = await cache.match(offlineFallbackPage);
+//         return cachedResp;
+//       }
+//     })());
+//   }
+// });
